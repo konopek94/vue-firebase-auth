2270e14eac51550a32b73645189e78c566596fa6
MKO <michalkonopka94@gmail.com>
2021-03-04 12:39:37 +0100

    set axios to global prop

--- src/main.js
+++ src/main.js
@@ -3,5 +3,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
 
-createApp(App).use(router, store).mount('#app')
+const app = createApp(App).use(router, store).mount('#app')
+app.config.globalProperties.$http = axios
