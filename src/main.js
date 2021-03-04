b92ebd4bd5e4e7b6516df7f382ca4856db7217b8
MKO <michalkonopka94@gmail.com>
2021-03-04 13:00:56 +0100

    add firebase config

--- src/main.js
+++ src/main.js
@@ -1,9 +1,22 @@
-import './plugins/axios'
+// import './plugins/axios'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from 'axios'
+import firebase from 'firebase/app'
 
-const app = createApp(App).use(router, store).mount('#app')
+var firebaseConfig = {
+  apiKey: "AIzaSyDYXv2gxOjvVIooPTHE38QYRaQHK1ImHxg",
+  authDomain: "fir-auth-a2397.firebaseapp.com",
+  projectId: "fir-auth-a2397",
+  storageBucket: "fir-auth-a2397.appspot.com",
+  messagingSenderId: "662368423357",
+  appId: "1:662368423357:web:37bd60eb187c8cc1db16d4"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const app = createApp(App).use(router, store)
 app.config.globalProperties.$http = axios
+app.mount('#app')
