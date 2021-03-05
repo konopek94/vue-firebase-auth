5e5e45c62185ac640715815150d4ca91be40e84c
MKO <michalkonopka94@gmail.com>
2021-03-05 13:51:46 +0100

    set register page

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,19 +1,42 @@
 <template>
-  <div>
-register
-  </div>
+  <div v-if="error" class="error">{{error.message}}</div>
+  <form @submit.prevent="pressed">
+    Register
+    <div class="email">
+      <input type="email" v-model="email" placeholder="email">
+    </div>
+    <div class="password">
+      <input type="password" v-model="password" placeholder="password">
+    </div>
+    <button type="submit"> Register</button>
+  </form>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+      error: null,
+    }
+  },
   setup () {
     
 
     return {}
+  },
+  methods: {
+    pressed() {
+      alert('submitted')
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.error {
+  color: red;
+  font-size: 1.2rem;
+}
 </style>
